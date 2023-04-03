886ea7001b99886220f895c51991538945ceb6ff
VP <51seer.vincent@gmail.com>
2023-04-04 01:58:13 +0800

    Remove vtt after downloading.
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- Windows/bot.ps1
+++ Windows/bot.ps1
@@ -4,6 +4,7 @@ function bestyt($url) {
 	echo $url
 	yt-dlp -F $url
     yt-dlp -f bestvideo+bestaudio $url --all-subs --write-sub --embed-sub --sub-format best --external-downloader aria2c --external-downloader-args "-x 16 -k 1M"
+    rm *.vtt
 }
 
 $curdir = (Get-Item .).FullName
